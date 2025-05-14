34bd5ace87241349c726436c0c71594cda46d655
uppalapatinandini <153113083+uppalapatinandini@users.noreply.github.com>
2025-05-14 17:11:24 +0530

    displaying name nandhu by using html

--- market.py
+++ market.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World</h1>'
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the about page of {username}'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
